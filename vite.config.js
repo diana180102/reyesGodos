71d19e2be96117f8b6d69a7637d000e56d0d8b66
Diana <diana101210@gmail.com>
2024-01-14 16:20:24 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reyesGodos/"
+   base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
+
 })
